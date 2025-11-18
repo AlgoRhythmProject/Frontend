5d60feabcc349e901a73894f1ea74184d55e52fc
andrucior <andrzej.kupiec123@gmail.com>
2025-11-18 21:21:52 +0100

    Revert "fix:docker"
    This reverts commit 22352abcabc554ca0d0171993f757e655ab3f401.

--- AlgoRhythm/src/api/apiClient.ts
+++ AlgoRhythm/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:7062/api",
+    baseURL: "https://localhost:7080/api",
     headers: {
         "Content-Type": "application/json",
         accept: "*/*",
