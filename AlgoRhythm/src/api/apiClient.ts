22352abcabc554ca0d0171993f757e655ab3f401
andrucior <andrzej.kupiec123@gmail.com>
2025-11-18 21:18:28 +0100

    fix:docker

--- AlgoRhythm/src/api/apiClient.ts
+++ AlgoRhythm/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://localhost:7080/api",
+    baseURL: "http://localhost:7062/api",
     headers: {
         "Content-Type": "application/json",
         accept: "*/*",
