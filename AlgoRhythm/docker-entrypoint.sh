284da1651f571e823221bccc616440cebd2998d8
andrucior <andrzej.kupiec123@gmail.com>
2026-01-30 20:17:09 +0100

    config fix

--- AlgoRhythm/docker-entrypoint.sh
+++ AlgoRhythm/docker-entrypoint.sh
@@ -6,6 +6,10 @@ if [ -f /usr/share/nginx/html/config.js ]; then
   sed -i "s|__API_BASE_URL__|${API_BASE_URL}|g" /usr/share/nginx/html/config.js
   sed -i "s|__ANALYZER_URL__|${ANALYZER_URL}|g" /usr/share/nginx/html/config.js
   sed -i "s|__VISUALIZER_URL__|${VISUALIZER_URL}|g" /usr/share/nginx/html/config.js
+  sed -i "s|__VISUALIZER_URL__|${GOOGLE_CLIENT_ID}|g" /usr/share/nginx/html/config.js
+
+  echo "Environment variables replaced successfully"
+  cat /usr/share/nginx/html/config.js
 else
   echo "Warning: config.js not found"
 fi
--- AlgoRhythm/src/config/global.ts
+++ AlgoRhythm/src/config/global.ts
@@ -3,7 +3,30 @@ declare global {
         ENV: {
             API_BASE_URL: string,
             ANALYZER_URL: string,
-            VISUALIZER_URL: string
+            VISUALIZER_URL: string,
+            GOOGLE_CLIENT_ID: string,
         };
     }
 }
+
+export const config = {
+    apiBaseUrl:
+        window.ENV?.API_BASE_URL && window.ENV.API_BASE_URL !== '__API_BASE_URL__'
+            ? window.ENV.API_BASE_URL
+            : import.meta.env.VITE_API_BASE_URL,
+
+    analyzerUrl:
+        window.ENV?.ANALYZER_URL && window.ENV.ANALYZER_URL !== '__ANALYZER_URL__'
+            ? window.ENV.ANALYZER_URL
+            : import.meta.env.VITE_ANALYZER_URL,
+
+    visualizerUrl:
+        window.ENV?.VISUALIZER_URL && window.ENV.VISUALIZER_URL !== '__VISUALIZER_URL__'
+            ? window.ENV.VISUALIZER_URL
+            : import.meta.env.VITE_VISUALIZER_URL,
+
+    googleClientId:
+        window.ENV?.GOOGLE_CLIENT_ID && window.ENV.GOOGLE_CLIENT_ID !== '__GOOGLE_CLIENT_ID__'
+            ? window.ENV.GOOGLE_CLIENT_ID
+            : import.meta.env.VITE_GOOGLE_CLIENT_ID,
+};
