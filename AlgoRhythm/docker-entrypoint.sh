e81dfd25df3f83a0f8ca4d99cc978949885ce4c0
andrucior <andrzej.kupiec123@gmail.com>
2026-02-04 20:31:07 +0100

    fix

--- AlgoRhythm/docker-entrypoint.sh
+++ AlgoRhythm/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ -f /usr/share/nginx/html/config.js ]; then
   sed -i "s|__API_BASE_URL__|${API_BASE_URL}|g" /usr/share/nginx/html/config.js
   sed -i "s|__ANALYZER_URL__|${ANALYZER_URL}|g" /usr/share/nginx/html/config.js
   sed -i "s|__VISUALIZER_URL__|${VISUALIZER_URL}|g" /usr/share/nginx/html/config.js
-  sed -i "s|__VISUALIZER_URL__|${GOOGLE_CLIENT_ID}|g" /usr/share/nginx/html/config.js
+  sed -i "s|__GOOGLE_CLIENT_ID__|${GOOGLE_CLIENT_ID}|g" /usr/share/nginx/html/config.js
 
   echo "Environment variables replaced successfully"
   cat /usr/share/nginx/html/config.js
